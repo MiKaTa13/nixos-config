527985afa221319c1a3d2203f97cd06c64c0abcc
fox-on-nix <nix@nomail.com>
2026-04-02 21:11:45 +0200

    chore(nix-cache): update Nix cache settings
    1. Switched the primary substituter from `cache.nixos-cuda.org` to `nix-community.cachix.org`.
    2. Updated the corresponding trusted public key for the new substituter.
    3. Removed commented-out lines for clarity.

--- hosts/nixos/os_modules/nix-cache.nix
+++ hosts/nixos/os_modules/nix-cache.nix
@@ -2,11 +2,11 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org"
-      # "https://cache.nixos-cuda.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      # "cache.nixos-cuda.org-1:mB9rLzDhxTpGmX2u4MSs7sKMHM9dV8b+I0bW255V2Zo="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
